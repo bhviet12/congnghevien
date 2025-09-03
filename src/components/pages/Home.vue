b40259b71eec930abd3d67cd87e42f968986cc06
bhviet1510 <bhviet1510@gmail.com>
2025-09-03 10:25:36 +0700

    update

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -3,9 +3,11 @@
 defineProps<{ lang: 'en' | 'vi'; setLang: (value: 'en' | 'vi') => void }>();
 import Section1 from '../Home/Section1.vue';
 import Section2 from '../Home/Section2.vue';
+import Section3 from '../Home/Section3.vue';
 </script>
 
 <template>
   <Section1 :lang="lang" />
   <Section2 :lang="lang"/>
+  <Section3 :lang="lang"/>
 </template>
